bbb37eb8bfbb1507d6687c7875aac39dad73f581
Iki Lo <ikilo@Ikis-MacBook-Air.local>
2023-11-24 20:06:28 +0200

    mobile adaptation

--- static/development/components/swiper/index.js
+++ static/development/components/swiper/index.js
@@ -1,6 +1,9 @@
+import Swiper, { Navigation } from "swiper";
+import 'swiper/css/navigation';
 import "./index.scss";
 
 new Swiper('.main__swiper-container', {
+  modules: [Navigation],
   navigation: {
     prevEl: '.swiper-btn-prev',
     nextEl: '.swiper-btn-next'
@@ -9,8 +12,8 @@ new Swiper('.main__swiper-container', {
 });
 
 new Swiper('.main__categories-swiper', {
-  slidesPerView: 5,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 5,
   navigation: {
     prevEl: '.category-btn-prev',
     nextEl: '.category-btn-next'
@@ -19,6 +22,14 @@ new Swiper('.main__categories-swiper', {
     1230: {
       slidesPerView: 7,
       spaceBetween: 30,
-    }  
+    },
+    800: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    }
   }
 });
